e1433761daf9f624b5ae76614a9ecc699c6cd748
Ram81 <ramramrakhya81@gmail.com>
2023-10-03 14:56:04 -0400

    [Utils] Add OVON training script

---
+++ scripts/train/1-ovon-ver.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# SBATCH --job-name=goat
+# SBATCH --output=slurm_logs/goat-ver-%j.out
+# SBATCH --error=slurm_logs/goat-ver-%j.err
+# SBATCH --gpus 1
+# SBATCH --nodes 1
+# SBATCH --cpus-per-task 10
+# SBATCH --ntasks-per-node 1
+# SBATCH --constraint=a40
+# SBATCH --partition=short
+# SBATCH --signal=USR1@100
+# SBATCH --requeue
+
+export GLOG_minloglevel=2
+export HABITAT_SIM_LOG=quiet
+export MAGNUM_LOG=quiet
+
+#MAIN_ADDR=$(scontrol show hostnames "${SLURM_JOB_NODELIST}" | head -n 1)
+# export MAIN_ADDR
+
+# source /srv/flash1/rramrakhya6/miniconda3/etc/profile.d/conda.sh
+# conda deactivate
+# conda activate goat
+
+export PYTHONPATH=/srv/flash1/rramrakhya3/fall_2023/habitat-sim/src_python/
+
+TENSORBOARD_DIR="tb/ovon/ver/resnetclip_rgb_text/seed_1/"
+CHECKPOINT_DIR="data/new_checkpoints/ovon/ver/resnetclip_rgb_text/seed_1/"
+DATA_PATH="data/datasets/ovon/hm3d/v5_final/"
+
+# srun 
+python -um goat.run \
+  --run-type train \
+  --exp-config config/experiments/ver_objectnav.yaml \
+  habitat_baselines.trainer_name="ver" \
+  habitat_baselines.num_environments=8 \
+  habitat_baselines.rl.policy.name=PointNavResNetCLIPPolicy \
+  habitat_baselines.rl.ddppo.train_encoder=False \
+  habitat_baselines.rl.ddppo.backbone=resnet50_clip_avgpool \
+  habitat_baselines.tensorboard_dir=${TENSORBOARD_DIR} \
+  habitat_baselines.checkpoint_folder=${CHECKPOINT_DIR} \
+  habitat.dataset.data_path=${DATA_PATH}/train/train.json.gz \
+  +habitat/task/lab_sensors@habitat.task.lab_sensors.clip_objectgoal_sensor=clip_objectgoal_sensor \
+  ~habitat.task.lab_sensors.objectgoal_sensor \
+  habitat.task.lab_sensors.clip_objectgoal_sensor.cache=data/clip_embeddings/ovon_stretch_final_cache.pkl \
+  habitat.task.measurements.success.success_distance=0.25 \
+  habitat.dataset.type="OVON-v1" \
+  habitat.task.measurements.distance_to_goal.type=OVONDistanceToGoal \
+  habitat.simulator.type="OVONSim-v0" 
