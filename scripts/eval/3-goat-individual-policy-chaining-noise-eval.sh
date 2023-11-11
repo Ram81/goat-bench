a5887f0c0b6c4fa39d561f444e3c3d3c371221f3
Ram81 <ramramrakhya81@gmail.com>
2023-11-10 23:12:46 -0500

    [Eval] Add noise probing evaluations

--- scripts/eval/3-goat-individual-policy-chaining-noise-eval.sh
+++ scripts/eval/3-goat-individual-policy-chaining-noise-eval.sh
@@ -25,40 +25,45 @@ conda activate goat
 export PYTHONPATH=/srv/flash1/rramrakhya3/fall_2023/habitat-sim/src_python/
 
 DATA_PATH="data/datasets/goat/v0.1.4"
-# eval_ckpt_path_dir="data/new_checkpoints/goat/ver/resnetclip_rgb_multimodal/seed_1/"
-# tensorboard_dir="tb/goat/ver/resnetclip_rgb_multimodal/seed_1/val_seen/"
-# split="val_seen"
+eval_ckpt_path_dir="data/new_checkpoints/goat/ver/chain_individual_skills/seed_1_v0.1.4/"
+tensorboard_dir="tb/goat/ver/chain_individual_skills/seed_1_v0.1.4/val_seen_noisy_analysis/"
+split="val_seen"
+
+# echo "Evaluating ckpt: ${eval_ckpt_path_dir}"
+# echo "Data path: ${DATA_PATH}/${split}/${split}.json.gz"
 
 srun python -um goat.run \
   --run-type eval \
   --exp-config config/experiments/ver_goat.yaml \
-  habitat_baselines.num_environments=20 \
+  habitat_baselines.num_environments=1 \
   habitat_baselines.trainer_name="goat_ppo" \
-  habitat_baselines.test_episode_count=10 \
-  habitat_baselines.rl.policy.name=PointNavResnetCLIPPolicy \
-  habitat_baselines.rl.ddppo.train_encoder=False \
-  habitat_baselines.rl.ddppo.backbone=resnet50_clip_avgpool \
+  habitat_baselines.rl.policy.name=GoatHighLevelPolicy \
   habitat_baselines.tensorboard_dir=$tensorboard_dir \
   habitat_baselines.eval_ckpt_path_dir=$eval_ckpt_path_dir \
+  habitat_baselines.checkpoint_folder=$eval_ckpt_path_dir \
   habitat.dataset.data_path="${DATA_PATH}/${split}/${split}.json.gz" \
-  +habitat/task/lab_sensors@habitat.task.lab_sensors.goat_goal_sensor=goat_goal_sensor \
+  +habitat/task/lab_sensors@habitat.task.lab_sensors.clip_objectgoal_sensor=clip_objectgoal_sensor \
+  +habitat/task/lab_sensors@habitat.task.lab_sensors.language_goal_sensor=language_goal_sensor \
+  +habitat/task/lab_sensors@habitat.task.lab_sensors.cache_instance_imagegoal_sensor=cache_instance_imagegoal_sensor \
+  +habitat/task/lab_sensors@habitat.task.lab_sensors.current_subtask_sensor=current_subtask_sensor \
   ~habitat.task.lab_sensors.objectgoal_sensor \
-  habitat.task.lab_sensors.goat_goal_sensor.object_cache=data/clip_embeddings/ovon_stretch_final_cache.pkl \
-  habitat.task.lab_sensors.goat_goal_sensor.image_cache=data/datasets/iin/hm3d/v2/${split}_embeddings/ \
-  habitat.task.lab_sensors.goat_goal_sensor.image_cache_encoder="CLIP_goat" \
-  habitat.task.lab_sensors.goat_goal_sensor.language_cache="data/datasets/languagenav/hm3d/v5_final/embeddings/${split}_clip_embedding.pkl" \
+  habitat.task.lab_sensors.clip_objectgoal_sensor.cache=data/clip_embeddings/goat/ovon_noisy_clip_embedding.pkl \
+  habitat.task.lab_sensors.cache_instance_imagegoal_sensor.cache=data/datasets/iin/hm3d/v2/val_seen_embeddings/ \
+  habitat.task.lab_sensors.cache_instance_imagegoal_sensor.image_cache_encoder="VC-1_noise" \
+  habitat.task.lab_sensors.language_goal_sensor.cache=data/datasets/languagenav/hm3d/v5_final/embeddings/val_seen_noisy_bert_embedding.pkl \
   habitat.task.measurements.success.success_distance=0.25 \
   habitat.dataset.type="Goat-v1" \
   habitat.task.measurements.distance_to_goal.type=GoatDistanceToGoal \
   habitat.task.measurements.success.type=GoatSuccess \
   habitat.task.measurements.spl.type=GoatSPL \
   habitat.task.measurements.soft_spl.type=GoatSoftSPL \
-  +habitat/task/measurements@habitat.task.measurements.goat_distance_to_goal_reward=goat_distance_to_goal_reward \
   ~habitat.task.measurements.distance_to_goal_reward \
+  +habitat/task/measurements@habitat.task.measurements.goat_distance_to_goal_reward=goat_distance_to_goal_reward \
   habitat.simulator.type="OVONSim-v0" \
   habitat_baselines.load_resume_state_config=False \
   habitat_baselines.eval.use_ckpt_config=False \
   habitat_baselines.eval.split=$split \
+  habitat_baselines.eval.should_load_ckpt=False \
   habitat_baselines.should_load_agent_state=False
 
 touch $checkpoint_counter
