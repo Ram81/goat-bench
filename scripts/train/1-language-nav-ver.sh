178e188c3adec83e57d0534452049cd3bfe47b95
Gunjan Chhablani <chhablani.gunjan@gmail.com>
2024-06-10 19:55:29 -0400

    Update 1-language-nav-ver.sh

--- scripts/train/1-language-nav-ver.sh
+++ scripts/train/1-language-nav-ver.sh
@@ -45,5 +45,6 @@ srun python -um goat.run \
   ~habitat.task.lab_sensors.objectgoal_sensor \
   habitat.task.lab_sensors.language_goal_sensor.cache=data/clip_embeddings/goat/language_nav_train_bert.pkl \
   habitat.task.measurements.success.success_distance=0.25 \
+  habitat.simulator.type="GoatSim-v0" 
   habitat.dataset.type="LanguageNav-v1" \
-  habitat.simulator.type="OVONSim-v0" 
+
